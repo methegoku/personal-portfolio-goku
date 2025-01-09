72da977007bf4de1b691a843abd4ffd75e6e375c
Gokul Elumalai <gokulelumalai46@gmail.com>
2025-01-09 10:07:19 +0530

    update

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -4,8 +4,7 @@ function Footer() {
   return (
     <section id="footer" className={styles.container}>
       <p>
-        &copy; 2024 methecreature. <br />
-        All rights reserved.
+        &copy; 2024 methecreature. All rights reserved.
       </p>
     </section>
   );
